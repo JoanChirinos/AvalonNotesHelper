60a8912b969596feace04e3be9ad6e768e014dc6
Joan Chirinos <jchirinos3201@gmail.com>
2025-09-07 23:14:39 -0400

    Can serve react from express for prod

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import cors from "cors";
-import avalonRoutes from "./routes/avalon.ts";
+import { fileURLToPath } from "url";
+import path from "path";
+import avalonRoutes from "./routes/avalon.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const buildPath = path.resolve(__dirname, "../../frontend/build");
+app.use(express.static(buildPath));
 
 // Middleware for logging requests
 app.use((req, res, next) => {
@@ -19,5 +27,10 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/avalon", avalonRoutes);
+
+// Serve index.html for any unknown route (for React Router)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
 
 app.listen(5000, () => console.log("Server running on port 5000"));
